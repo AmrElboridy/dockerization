6081057ef38d42c75ac07d0f5d44e8fc28cdcd35
amr.elboridy <amr.elboridy@brightskiesinc.com>
2022-06-02 16:07:00 +0200

    initial commit

---
+++ drkiq/test/jobs/pages_controller_test.rb
@@ -0,0 +1,10 @@
+#require 'test_helper'
+
+#class PagesControllerTest < ActionDispatch::IntegrationTest
+ # test "should get home" do
+ # test "should_get_home" do
+
+  #  get "/"
+   # assert_response :success
+ # end
+#end
